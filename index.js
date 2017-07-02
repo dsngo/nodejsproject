5f799a3e31193a03d537c02cf6974be04b5b7e22
geart891 <daniel.dungngo@gmail.com>
2017-07-02 15:20:24 +0700

    restructure

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // INITIAL CONFIGURATION ===================
-const { app, sPORT, sIP, sLog, staticD } = require('./controllers/handleVars')
+const { app, staticD, sPORT, sIP, sLog } = require('./controllers/handleVars')
 const dbConfig = require('./controllers/databaseConfigs');
 const authConfig = require('./controllers/authConfigs');
 const { urlEncMw, flashMw } = require('./controllers/middlewares');
-const indexRoute = require('./routes/index');
-const campgroundRoute = require('./routes/campgrounds');
+const landingRoute = require('./routes/landing');
+const blogRoute = require('./routes/blog');
 const commentRoute = require('./routes/comments');
-// DATABSE CONFIGURATION ===================
+// DATABASE CONFIGURATION ==================
 dbConfig();
 // AUTHENTICATION CONFIGURATION ============
 authConfig();
@@ -16,9 +16,9 @@ app.set('views', `${__dirname}/views`);
 app.use(staticD);
 app.use(flashMw);
 // ROUTES CONFIGURATION ====================
-app.use('/', indexRoute);
-app.use('/index', campgroundRoute);
-app.use('/index/:id/comments', urlEncMw, commentRoute);
+app.use('/', landingRoute);
+app.use('/blog', blogRoute);
+app.use('/blog/:id/comments', urlEncMw, commentRoute);
 // FOOTER CONFIGURATION ====================
 app.get('*', (rq, rs) => rs.send('404'));
 app.listen(sPORT, sIP, sLog);
